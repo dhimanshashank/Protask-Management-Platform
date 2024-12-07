b6661c6f8f3615547c8b5c7f9cb2f81be7d76109
dhimanshashank <dhimanshashank15@gmail.com>
2024-12-07 10:46:07 +0530

    SpringBoot Microservice first commit

---
+++ backend/project-managament-service/src/main/java/com/protask/project_managament_service/ProjectManagamentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.protask.project_managament_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagamentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagamentServiceApplication.class, args);
+	}
+
+}
---
+++ backend/project-managament-service/src/test/java/com/protask/project_managament_service/ProjectManagamentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protask.project_managament_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagamentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
