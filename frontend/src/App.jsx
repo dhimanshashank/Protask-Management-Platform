bff3e27bc30aa6d811174574339d089016d2c48b
dhimanshashank <dhimanshashank15@gmail.com>
2024-12-21 23:57:32 +0530

    Logo created and added, along with tailwind configurations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
+import Home from "./pages/Home"
+
 function App() {
 
-  return (
-    <div>
-      <h1>Frontend</h1>
-    </div>
-  )
+  return <>
+      <Home />
+    </>
 }
 
 export default App
